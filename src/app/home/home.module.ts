c215f7495ed93206feb427f7922c93625e7e75b6
Kolawole Olulana <kolawolegolulana@gmail.com>
2021-05-28 11:55:24 +0200

    first commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
+import { Angular4PaystackModule } from 'angular4-paystack';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
 
 @NgModule({
   imports: [
+    Angular4PaystackModule.forRoot('pk_test_beaf8a50a1e76261f013e0201f1fa63e8b67d353'),
+
     CommonModule,
     FormsModule,
     IonicModule,
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PaystackOptions } from 'angular4-paystack';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage  {
+  title = '';
 
+  options: PaystackOptions = {
+    amount: 50000,
+    email: 'user@mail.com',
+    ref: `${Math.ceil(Math.random() * 10e10)}`,
+    currency:"ZAR"
+  }
   constructor() {}
+
+  paymentInit() {
+    console.log('Payment initialized');
+  }
+
+  paymentDone(ref: any) {
+    this.title = 'Payment successfull';
+    console.log(this.title, ref);
+  }
+
+  paymentCancel() {
+    console.log('payment failed');
+  }
+
 
 }
